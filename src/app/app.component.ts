1eaee8f029e0ce3c7e18eab9bd93cf4e5e5a527a
eshwar.more <eshwar.more@phonepe.com>
2019-03-24 11:41:13 +0530

    all checked in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bmw7';
+  columns = ["name", "sirname", "age"];
+  allColumns = ["name", "sirname", "age"];
+  toggle = false;
+  data = [
+    {
+      name: "Mr. 1",
+      age: 25,
+      sirname: "Bond"
+    },
+    {
+      name: "Ms. 2",
+      age: 32,
+      sirname: "Bond"
+    }
+  ];
+  allData = this.data;
+  criteria = "";
+
+  public showHide(): void {
+    if (!this.toggle) {
+      this.columns = this.columns.filter((col) => col != "age");
+    } else {
+      this.columns = this.allColumns;
+    }
+    this.toggle = !this.toggle;
+  }
+
+  public filter(): void {
+    this.data = this.allData.filter((row) => {
+      let found = false;
+      for (let key of this.columns) {
+        console.log(key);
+        if ((row[key] + "").includes(this.criteria)) {
+          found = true;
+        }
+      }
+      return found;
+    });
+  }
 }
